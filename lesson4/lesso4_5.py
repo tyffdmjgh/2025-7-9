c865423e016442b32aec00d051d9de04f27bdf90
tyffdmjgh <s110003@syups.tp.edu.tw>
2025-07-22 08:22:49 +0800

    modify

--- lesson4/lesso4_5.py
+++ lesson4/lesso4_5.py
@@ -1,2 +1,14 @@
-import crawl4ai
-print(crawl4ai.__version__.__version__)
+from playwright.async_api import async_playwright
+import asyncio
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.goto('https://example.com')
+        await page.wait_for_selector('p') #等待元素載入
+        content = await page.inner_text('p')
+        print(content)
+        await browser.close()
+    
+asyncio.run(main())
